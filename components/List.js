b97edd836a8b4506b838678a83f15a0a304455ea
Tatu <tatu.d.ihaksi@gmail.com>
2022-01-16 04:43:33 +0200

    task b done

--- components/List.js
+++ components/List.js
@@ -3,9 +3,6 @@ import {FlatList} from 'react-native';
 import {baseUrl} from '../utils/variables';
 import ListItem from './ListItem';
 
-// const dataUrl ='https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
-// const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
-
 const List = () => {
   const [mediaArray, setMediaArray] = useState([]);
   const loadMedia = async (start = 0, limit = 10) => {
@@ -17,7 +14,7 @@ const List = () => {
         throw Error(response.statusText);
       }
       const json = await response.json();
-      const media = Promise.all(
+      const media = await Promise.all(
         json.map(async (item) => {
           const response = await fetch(baseUrl + 'media/' + item.file_id);
           const mediaData = await response.json();
@@ -25,13 +22,14 @@ const List = () => {
         })
       );
       setMediaArray(media);
+      console.log(mediaArray);
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    loadMedia();
+    loadMedia(0, 5);
   }, []);
 
   return (
